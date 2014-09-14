aae88e4472df0a6e62ad33e77ef91980e12e7bdb
Roy Magen <magenroy@gmail.com>
2014-09-14 21:58:38 +0000

    first commit

---
+++ CombIO.hs
@@ -0,0 +1,49 @@
+module Main (main) where
+
+import System.Environment (getArgs)
+import Control.Monad ((>=>))
+
+import Combinations (combine, combinations, slice, groupPermutations)
+
+-- make structure generator
+--  requires finding way to properly group the strings
+
+options =
+    ("s", lcomb):("-strings", lcomb):
+    ("t", lslice):("-terms", lslice):
+    ("g", lgroup):("-group", lgroup):
+    []
+
+
+help :: String
+help = "Finds all combinations of elements in a list of lists"
+    ++ "\n USAGE: combine [FLAG] [ARGUMENTS]"
+    ++ "\n\t run with no arguments to use standard input (list of lines containing lists of words)"
+    ++ "\n\t run with filenames and no options to use the contents of that file (list of lines containing lists of words)"
+    ++ "\n OPTIONS"
+    ++ "\n\t -s --strings \t combine argument strings"
+    ++ "\n\t -t --terms \t count like strings resulting from combining arguments (strings are compared based on the sum of the numerical representations of their characters)"
+    ++ "\n\t -g --group \t group like terms"
+
+fcomb :: String -> IO ()
+fcomb = putStr . unlines . map unwords . combine . map words . lines
+
+lcomb :: [String] -> IO ()
+lcomb = putStrLn . unwords . combine
+
+enumSum :: Enum a => [a] -> Int
+enumSum = sum . map fromEnum
+
+lslice :: Enum a => [[a]] -> IO ()
+lslice = putStrLn . unwords . map show . (slice enumSum)
+
+lgroup = putStr . unlines . map unwords . fst . unzip . map unzip . groupPermutations enumSum . combine -- should indicate how many elements on each line
+
+combIO :: [String] -> IO ()
+combIO [] = getContents >>= fcomb
+combIO (('-':flag):args) = dispatch $ lookup flag options
+    where dispatch Nothing = putStrLn help
+	  dispatch (Just opt) = opt args
+combIO fNs = sequence_ $ map (readFile >=> fcomb) fNs
+
+main = getArgs >>= combIO
---
+++ Combinations.hs
@@ -0,0 +1,32 @@
+module Combinations where
+
+import Data.List (group, groupBy, sort, sortBy)
+import Control.Applicative (liftA2, (<*>))
+
+
+combine :: [[a]] -> [[a]]
+combine = foldr (liftA2 (:)) [[]] -- (\lst acc -> (:) <$> lst <*> acc)
+
+combinations :: [[a]] -> [[[a]]]
+combinations = scanl (flip $ liftA2 (:)) [[]] -- makes right side up simpleces
+
+
+collectPermutations :: Ord b => ([a] -> b) -> [[a]] -> [[b]]
+collectPermutations = ((group . sort) .) . map
+
+groupPermutations :: Ord b => (a -> b) -> [a] -> [[(a, b)]]
+groupPermutations f = let check a b = (snd a == snd b, compare (snd a) (snd b)) in
+    groupBy ((fst .) . check) .
+    sortBy ((snd .) . check) .
+    map (\x -> (x, f x))
+
+likeTerms :: Ord b => ([a] -> b) -> [[a]] -> [Int]
+likeTerms = (map length .) . collectPermutations
+likeTerms' f = (map length .) . groupPermutations $ f -- why is this so much slower? (are fst and snd really that slow?)
+
+slice :: Ord b => ([a] -> b) -> [[a]] -> [Int]
+slice f = likeTerms f . combine
+
+struct :: Ord b => ([a] -> b) -> [[a]] -> [[Int]]
+struct f = map (likeTerms f) . combinations
+struct' f = map (likeTerms' f) . combinations
